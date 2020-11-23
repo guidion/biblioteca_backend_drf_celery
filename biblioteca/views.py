afd1ece9b76ab47aa91f881b1c7ee6dddb0e3f42
Guídion Díaz <guidion.diaz@tektontech.com>
2020-11-22 19:27:34 -0500

    Terminados servicios y URLs para Libros, Editoriales y Autores

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,6 +1,52 @@
-"""
-Biblioteca Views
-"""
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from biblioteca.serializers import AuthorSerializer, PublisherSerializer, BookSerializer
 
-# Create your views here.
+
+class AuthorList(ListCreateAPIView):
+    serializer_class = AuthorSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+
+class AuthorDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = AuthorSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+
+class PublisherList(ListCreateAPIView):
+    serializer_class = PublisherSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+
+class PublisherDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = PublisherSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+
+class BookList(ListCreateAPIView):
+    serializer_class = BookSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+
+class BookDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = BookSerializer
+    model = serializer_class.Meta.model
+    queryset = model.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
